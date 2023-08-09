381abf6f94ff5787c435ad8a18523c84f792a372
João Falcão <jgabfalcao@gmail.com>
2023-08-09 16:24:18 -0300

    fix: Pasta JS e inserindo paginação no site

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function moveToQuestion() {
+  window.location.href = "question.html";
+}
+
+function moveToSecret() {
+  window.location.href = "secret.html";
+}
+
+function backToIndex() {
+  window.location.href = "index.html";
+}
